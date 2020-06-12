a5cad186cb6f874d6a7ef1feb638ee44f9a13da8
denarioresearch <31984275+denarioresearch@users.noreply.github.com>
2020-06-12 12:25:12 +0300

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -10,14 +10,14 @@ signals.drop(['Unnamed: 0'], inplace=True, axis=1)
 
 
 t1 = pd.Series(signals['Date'].values, index=signals['Date'].values - pd.Timedelta('14 days'),name='t1')
-t1.to_csv('XBTUSD_t1')
+t1.to_csv('XBTUSD_t1.csv')
 def getIndMatrix(barIx,t1):
     indM = pd.DataFrame(0, index=barIx, columns=range(t1.shape[0]))
     for i, (t0,t1) in enumerate(t1.iteritems()):
         indM.loc[t0:t1,i]=1
     return indM
 indM= getIndMatrix(signals['Date'],t1)
-indM.to_csv('XBTUSD_indM')
+indM.to_csv('XBTUSD_indM.csv')
 def getAvgUniquness(indM):
     c = indM.sum(axis=1)
     u = indM.div(c,axis=0)
